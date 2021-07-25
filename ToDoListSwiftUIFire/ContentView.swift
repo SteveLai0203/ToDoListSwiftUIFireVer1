a217af02bb86f347a82307ea0990536a83923cb2
lzrong0203 <lzrong0203@gmail.com>
2021-07-25 23:59:27 +0800

    Initial Commit

---
+++ ToDoListSwiftUIFire/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ToDoListSwiftUIFire
+//
+//  Created by Steve Lai on 2021/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
